69432b6cf12c91404b9aabe42bb0210a17d38182
Sprenger <fel_spr@uni-bremen.de>
2022-09-02 20:54:15 +0200

    server8

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['8G9lFEF53z8IAuYilfv5yw==','YwuLyw4qhhH9c1J7NQrvBA==','FJthB3Kt/tF2BH3QPXOyLQ==','Yv+HSCy4o9iX27q1EAOdPg==']),
   },
 });
